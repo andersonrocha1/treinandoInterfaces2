01d2457a619db1897f9a1c92bcdeafeb53324c45
andersonrocha1 <anderson.tec.informacao@gmail.com>
2023-01-07 14:35:27 -0300

    Enviando os arquivos e adicinando .gitignore

---
+++ src/application/Program.java
@@ -0,0 +1,48 @@
+package application;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Contract;
+import entities.Installment;
+import services.ContractService;
+import services.PaypalService;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		
+		System.out.println("Entre com os dados do contrato: ");
+		System.out.print("Número: ");
+		int number = sc.nextInt();
+		System.out.print("Data: (dd/MM/yyyy)");
+		LocalDate date = LocalDate.parse(sc.next(), fmt );
+		System.out.print("Valor do contrato: ");
+		double totalValue = sc.nextDouble();
+		
+		Contract ctc = new Contract(number, date, totalValue);
+		System.out.println("Entre com o numero de parcelas: ");
+		int n = sc.nextInt();
+		
+		ContractService contractService = new ContractService(new PaypalService());
+		contractService.processContract(ctc, n);
+		
+		System.out.println("Parcelas : ");
+		for(Installment installment : ctc.getInstallment()) {
+			System.out.println(installment);
+		}
+		
+		
+		sc.close();
+
+	}
+
+}
